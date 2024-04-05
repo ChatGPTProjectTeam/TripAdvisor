48db6786860ae16166755c4e52bf28233661553c
kendsi <tjrwls1832@gmail.com>
2024-04-05 20:01:25 +0900

    initialize project

---
+++ src/test/java/com/tripper/Tripper/TripperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripper.Tripper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
