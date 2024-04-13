c4c6cd0532e88984b7e64ad0e76a987763d35505
sanghunmoon <lego0142@naver.com>
2024-04-14 01:00:58 +0900

    alembic

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,13 +1,10 @@
-import sys
 from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-sys.path = ['', '..'] + sys.path[1:]  # for alembic to find the backend module
-from backend.database import Base
-from backend.settings import settings
+from backend import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -18,9 +15,11 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", settings.SQLALCHEMY_DATABASE_URL)
-target_metadata = Base.metadata
-
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -66,7 +65,9 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
