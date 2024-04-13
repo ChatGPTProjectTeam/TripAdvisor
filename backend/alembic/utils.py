c51ae37e1ebaf75c56565a41e37f953bd0f2906b
sanghunmoon <lego0142@naver.com>
2024-04-13 18:07:12 +0900

    alchemy

--- backend/alembic/utils.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-import pytz
-
-
-def get_now() -> datetime:
-    korea_time_zone = pytz.timezone("Asia/Seoul")
-    return datetime.now(korea_time_zone)
--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = "mysql://tripper:*tripper@localhost:3306/tripper"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
