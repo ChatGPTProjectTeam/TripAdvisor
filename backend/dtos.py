a83ad23b2fd9b1ce1424b1cd88e33da943fc5980
jaehyun Shin <skwent77@gmail.com>
2024-05-06 14:04:24 +0900

    FormRequest DTO, PlanDTO 수정

--- backend/dtos.py
+++ backend/dtos.py
@@ -1,42 +1,39 @@
-from datetime import date
-from typing import Literal
+from datetime import date, datetime
 
 from pydantic import BaseModel, Field
 
 
-# Deprecated
-class TripInfo(BaseModel):
-    mbti: str
-    province: str
-    days: int
-    start_date: date
-    trip_member_num: int
-    trip_style_text: str
-
-
-class PlaneInfo(BaseModel):
+class PlaneInfoDTO(BaseModel):
     """
-    # 스카이스캐너에서 가져온 비행 정보 예시 필드
+    skyscanner plane info
     """
 
-    price: str  # 가격
-    origin: str  # 출발지
-    destination: str  # 목적지
-    departure: str  # 출발일
-    arrival: str  # 도착일
-    airline: str  # 항공사
+    price: str = Field(description="가격")
+    origin: str = Field(description="출발지")
+    destination: str = Field(description="목적지")
+    departure: str = Field(description="출발일")
+    arrival: str = Field(description="도착일")
+    airline: str = Field(description="항공사")
+
+    class Config:
+        from_attributes = True
 
 
-class AccommodationInfo(BaseModel):
+class AccommodationInfoDTO(BaseModel):
     """
-    스카이스캐너에서 가져온 숙소 정보 예시 필드
+    skyscanner accomodation info
     """
 
-    name: str  # 숙소 이름
-    stars: str  # 몇 성 호텔인지 (없을 경우에 "no_stars")
-    lowest_price: str  # 여러 SKYSCANNER 제휴 숙소 앱 중 해당 숙소를 가장 싸게 예약할 수 있는 가격
-    rating: str  # 리뷰 평균 평점
-    location: str  # 주소
+    name: str = Field(description="숙소 이름")
+    stars: str = Field(description="몇 성 호텔인지 (없을 경우에 'no_stars')")
+    lowest_price: str = Field(
+        description="여러 SKYSCANNER 제휴 숙소 앱 중 해당 숙소를 가장 싸게 예약할 수 있는 가격"
+    )
+    rating: str = Field(description="리뷰 평균 평점")
+    location: str = Field(description="주소")
+
+    class Config:
+        from_attributes = True
 
 
 class RestaurantInfo(BaseModel):
@@ -50,32 +47,29 @@ class RestaurantInfo(BaseModel):
     cuisine: str
     rating: float
 
-
-class TimePlanDTO(BaseModel):
-    activity: str = ""
-    restaurant: RestaurantInfo | None
-
-
-class DayPlanDTO(BaseModel):
-    date: date
-    morning: TimePlanDTO
-    afternoon: TimePlanDTO
-    evening: TimePlanDTO
+    class Config:
+        from_attributes = True
 
 
 class PlanComponentDTO(BaseModel):
     component_id: int
     component_type: str
-    plane_info: PlaneInfo | None
-    accommodation_info: AccommodationInfo | None
-    day_plan_list: list[DayPlanDTO] = []
+    plane_info: PlaneInfoDTO | None
+    accommodation_info: AccommodationInfoDTO | None
+    activity: str | None
+
+    class Config:
+        from_attributes = True
 
 
 class PlanDTO(BaseModel):
-    plan_id: int | None
+    trip_plan_id: int | None
     province: str
-    created_at: date = Field(default_factory=date.today)
+    created_at: datetime
     plan_component_list: list[PlanComponentDTO]
+
+    class Config:
+        from_attributes = True
 
 
 """
@@ -98,3 +92,24 @@ class PlanListResponseDTO(BaseModel):
 
 class UserInput(BaseModel):
     msg: str
+
+
+# Deprecated
+class TripInfo(BaseModel):
+    mbti: str
+    province: str
+    days: int
+    start_date: date
+    trip_member_num: int
+    trip_style_text: str
+
+    @classmethod
+    def from_form_request_dto(cls, form_request_dto: FormRequestDTO) -> "TripInfo":
+        return cls(
+            mbti=form_request_dto.mbti,
+            province=form_request_dto.province,
+            days=form_request_dto.days,
+            start_date=form_request_dto.start_date,
+            trip_member_num=form_request_dto.trip_member_num,
+            trip_style_text=form_request_dto.trip_style_text,
+        )
