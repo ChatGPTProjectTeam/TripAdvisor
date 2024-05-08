6df108f7722872c354f27ba79cc22627882f3c1d
sanghunmoon <lego0142@naver.com>
2024-05-08 19:32:35 +0900

    skyscanner 에러 대응

--- backend/services/plan.py
+++ backend/services/plan.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 from backend.database import SessionLocal
 from backend.dtos import TripInfo, PlanDTO
-from backend.models import Plan, PlanComponent
+from backend.models import Plan, PlanComponent, PlaneInfo, AccommodationInfo
 
 if TYPE_CHECKING:
     from backend.services import SkyscannerService, GPTService
@@ -24,15 +24,15 @@ class PlanService:
             plans = session.query(Plan).all()
             plan_list = [PlanDTO.from_orm(plan) for plan in plans]
         return plan_list
-    
+
     def get_plan(self, plan_id: int) -> PlanDTO:
         with SessionLocal() as session:
             plan = session.query(Plan).filter(Plan.trip_plan_id == plan_id).all()
             return PlanDTO(
-                trip_plan_id = plan[0].trip_plan_id,
-                province = plan[0].province,
-                created_at = plan[0].created_at,
-                plan_component_list = plan[0].plan_component_list
+                trip_plan_id=plan[0].trip_plan_id,
+                province=plan[0].province,
+                created_at=plan[0].created_at,
+                plan_component_list=plan[0].plan_component_list,
             )
 
     def initiate_plan(self, trip_info: TripInfo):
@@ -54,7 +54,44 @@ class PlanService:
             activities = executor.submit(
                 self.gpt_service.generate_activities, trip_info
             )
-        from_plane_info, to_plane_info, accommodation_info = skyscanner_result.result()
+        try:
+            from_plane_info, to_plane_info, accommodation_info = (
+                skyscanner_result.result()
+            )
+        except Exception as e:
+            print(e)
+            from_plane_info = PlaneInfo(
+                price="0",
+                origin="인천국제공항",
+                destination="일본",
+                departure="비행기 정보를 불러오지 못했어요. 나중에 다시 시도해주세요.",
+                arrival="",
+                airline="",
+            )
+            to_plane_info = PlaneInfo(
+                price="0",
+                origin="일본",
+                destination="인천국제공항",
+                departure="비행기 정보를 불러오지 못했어요. 나중에 다시 시도해주세요.",
+                arrival="",
+                airline="",
+            )
+            accommodation_info = AccommodationInfo(
+                name="숙소 정보를 불러오지 못했어요. 나중에 다시 시도해주세요",
+                stars="1",
+                lowest_price="",
+                rating="",
+                location="Location: 9-15 togano-cho, Kita-ku, 530-0056 Osaka, Japan",
+            )
+            with SessionLocal() as session:
+                session.add(from_plane_info)
+                session.add(to_plane_info)
+                session.add(accommodation_info)
+                session.commit()
+                session.refresh(from_plane_info)
+                session.refresh(to_plane_info)
+                session.refresh(accommodation_info)
+
         activities = activities.result()
 
         from_plane_component = PlanComponent(
