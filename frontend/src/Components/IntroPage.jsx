277f4a7e969d6324e280b9208765e33eed44e88d
yongjoocho <cho.y.j1997@gmail.com>
2024-05-09 14:57:50 +0900

    마크다운 구현완료, 인트로 애니메이션 추가완료, 라이트 모드 배경색깔 수정완료

--- frontend/src/Components/IntroPage.jsx
+++ frontend/src/Components/IntroPage.jsx
@@ -1,40 +1,29 @@
-import React, {useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './IntroPage.module.css';
 import ChatCreateButton from "./ChatButton.jsx";
-import chatData from "../frontDB/chatLog.json";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import {ChatIntroCreateButton} from "./ChatButton.jsx"
+
 
 function IntroPage() {
+  const navigate = useNavigate();
+  const [fadeIn, setFadeIn] = useState(false);
 
+  useEffect(() => {
+    // Trigger fade-in animation after component mounts
+    setFadeIn(true);
+  }, []);
 
-
-
-
-const ChatCreateButton = () => { // Use '=' instead of '=>' for function declaration
-    // const [chats, setChats] = useState(chatData.chats);
-    const navigate = useNavigate();
-    function handleCreateChat() {
-        navigate('/create_form');
-    }
-
-    return (
-        <div style={{display: 'flex'}}>
-          <button onClick={handleCreateChat} className={`button-64 ${styles.sidebarButton}`}>
-            <span className="text">가고 싶어</span>
-          </button>
-        </div>
-    );
-}
-
+  function handleCreateChat() {
+    navigate('/create_form');
+  }
 
   return (
     <div style={{ flex: '4', position: 'relative' }}>
-      <div className={styles['canvas-container']} style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
-      </div>
-      <div style={{ position: 'absolute', top: '40%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1 }}>
+      <div className={`${styles.introContainer} ${fadeIn ? styles.fadeIn : ''}`}>
         <h1 style={{ color: 'white' }}>일본가고 싶니?</h1>
-        <div style={{display:'flex', justifyContent:'center', marginTop:'80px'}}>
-          <ChatCreateButton/>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '80px' }}>
+          <ChatIntroCreateButton />
         </div>
       </div>
     </div>
