84eb3a503392f46cbbb63bc2d2647e922dab0c3e
yongjoocho <cho.y.j1997@gmail.com>
2024-06-20 23:29:11 +0900

    가이드 추가

--- frontend/src/Components/GuideLinePopUp.jsx
+++ frontend/src/Components/GuideLinePopUp.jsx
@@ -4,7 +4,10 @@ import React, {useEffect, useState} from 'react';
 const GuideLinePopUp = () => {
     return (
         <div>
-            가이드라인입니다.
+            <div>
+                <img src="/guide.png" style={{width: '100%', height: '100%'}}
+                     alt="Description of the image"/>
+            </div>
         </div>
     )
 }
