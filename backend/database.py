a088a14456c75aa0595adbcbe6574ace396e0b22
unknown <skwent77@gmail.com>
2024-06-26 16:32:17 +0900

    etc code

--- backend/database.py
+++ backend/database.py
@@ -10,3 +10,12 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+app = FastAPI()
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, db: Session = Depends(database.get_db)):
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
